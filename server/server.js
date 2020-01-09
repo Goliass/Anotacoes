97922ae96f13854cd0dbe026113a49d653deb777
Goliass <sauthier.elias@gmail.com>
2020-01-09 16:08:15 -0300

    incluída a possibilidade de informar o número da porta por parâmetro, ao executar/iniciar o servidor

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
-const port = 9092;
+const app = require('./src/config/express');
 const CustomDate = require('./src/utils/CustomDate');
 
-const app = require('./src/config/express');
+const argPort = parseInt(process.argv[2]);
+let port = 3000;
+
+if (argPort && !isNaN(argPort)) {
+  port = argPort;
+}
 
 app.listen(port, () => {
   console.log(`Server running on port ${port} - ${CustomDate.currentTime()}`);
